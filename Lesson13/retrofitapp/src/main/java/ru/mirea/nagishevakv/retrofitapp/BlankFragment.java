52400023f38ccd9af386f0b4899b3466c1d6bf49
Ксения Нагишева <knagisheva@gmail.com>
2026-09-18 17:02:44 +0300

    Fix

--- Lesson13/retrofitapp/src/main/java/ru/mirea/nagishevakv/retrofitapp/BlankFragment.java
+++ Lesson13/retrofitapp/src/main/java/ru/mirea/nagishevakv/retrofitapp/BlankFragment.java
@@ -1,20 +1,36 @@
 package ru.mirea.nagishevakv.retrofitapp;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BlankFragment extends Fragment {
 
+    public static final String TAG = "BlankFragment";
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     private static final String ARG_NUMBER_STUDENT = "my_number_student";
+
     private int numberStudent;
+    private RecyclerView recyclerView;
+    private TodoAdapter todoAdapter;
+    private ApiService apiService;
 
     public BlankFragment() {
         // Required empty public constructor
@@ -34,13 +50,71 @@ public class BlankFragment extends Fragment {
         if (getArguments() != null) {
             numberStudent = getArguments().getInt(ARG_NUMBER_STUDENT);
         }
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiService = retrofit.create(ApiService.class);
     }
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        Log.d(BlankFragment.class.getSimpleName(), "Student Number: " + numberStudent);
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        
+        Log.d(TAG, "Student Number: " + numberStudent);
+        
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        
+        loadTodos();
+        
+        return view;
+    }
+
+    private void loadTodos() {
+        Call<List<Todo>> call = apiService.getTodos();
+        call.enqueue(new Callback<List<Todo>>() {
+            @Override
+            public void onResponse(Call<List<Todo>> call, Response<List<Todo>> response) {
+                if (response.isSuccessful() && response.body() != null && isAdded()) {
+                    List<Todo> todos = response.body();
+                    todoAdapter = new TodoAdapter(getContext(), todos, (todo, isChecked) -> {
+                        todo.setCompleted(isChecked);
+                        updateTodo(todo);
+                    });
+                    recyclerView.setAdapter(todoAdapter);
+                } else {
+                    Log.e(TAG, "onResponse: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Todo>> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
+        });
+    }
+
+    private void updateTodo(Todo todo) {
+        apiService.updateTodo(todo.getId(), todo).enqueue(new Callback<Todo>() {
+            @Override
+            public void onResponse(Call<Todo> call, Response<Todo> response) {
+                if (response.isSuccessful() && isAdded()) {
+                    Toast.makeText(getContext(), 
+                            "Updated: " + response.body().getTitle(),
+                            Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "Updated successfully: " + response.code());
+                } else {
+                    Log.e(TAG, "Update failed: " + response.code());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Todo> call, Throwable t) {
+                Log.e(TAG, "Update error: " + t.getMessage());
+            }
+        });
     }
 }
--- Lesson13/retrofitapp/src/main/java/ru/mirea/nagishevakv/retrofitapp/MainActivity.java
+++ Lesson13/retrofitapp/src/main/java/ru/mirea/nagishevakv/retrofitapp/MainActivity.java
@@ -1,32 +1,14 @@
 package ru.mirea.nagishevakv.retrofitapp;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = "MainActivity";
-    public static final String BASE_URL =
-            "https://jsonplaceholder.typicode.com/";
-    private RecyclerView recyclerView;
-    private TodoAdapter todoAdapter;
-    private ApiService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,61 +28,5 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.fragment_container_view, fragment)
                     .commit();
         }
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        apiService = retrofit.create(ApiService.class);
-        
-        loadTodos();
-    }
-
-    private void loadTodos() {
-        Call<List<Todo>> call = apiService.getTodos();
-        call.enqueue(new Callback<List<Todo>>() {
-            @Override
-            public void onResponse(Call<List<Todo>> call, Response<List<Todo>> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    List<Todo> todos = response.body();
-                    todoAdapter = new TodoAdapter(MainActivity.this, todos, (todo, isChecked) -> {
-                        todo.setCompleted(isChecked);
-                        updateTodo(todo);
-                    });
-                    recyclerView.setAdapter(todoAdapter);
-                } else {
-                    Log.e(TAG, "onResponse: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<List<Todo>> call, Throwable t) {
-                Log.e(TAG, "onFailure: " + t.getMessage());
-            }
-        });
-    }
-
-    private void updateTodo(Todo todo) {
-        apiService.updateTodo(todo.getId(), todo).enqueue(new Callback<Todo>() {
-            @Override
-            public void onResponse(Call<Todo> call, Response<Todo> response) {
-                if (response.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, 
-                            "Updated: " + response.body().getTitle(),
-                            Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Updated successfully: " + response.code());
-                } else {
-                    Log.e(TAG, "Update failed: " + response.code());
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Todo> call, Throwable t) {
-                Log.e(TAG, "Update error: " + t.getMessage());
-            }
-        });
     }
 }
